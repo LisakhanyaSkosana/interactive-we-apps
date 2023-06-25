2d487a58f28085339e94095ec325a44c974bc000
Lisakhanya Skosana <lindiwelisakhaya@gmail.com>
2023-06-25 17:01:30 +0200

    update

---
+++ IWA_2/script.js
@@ -0,0 +1,19 @@
+// challenge 1
+const user = "Lisakhanya";
+const surname = "Skosana";
+
+console.log(user,surname);
+//
+
+
+// challenge 2
+const date = "23/06/2023";
+console (date);
+//
+
+
+// challenge 3
+
+/* it is important to let user know when they can close the page */
+console.warn 
+//
